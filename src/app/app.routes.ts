818ea2bb345972c5ce909e6c7ae960c06be73706
Lorran Ribeiro <lorrranrb16@gmail.com>
2026-08-11 10:15:07 -0300

    Guards e Seguranca de Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,23 @@
 import { Routes } from '@angular/router';
-  export const routes: Routes = [
-    {
-      path: '',
-      loadComponent: () => import('./features/home/home/home').then((m) => m.Home),
-    },
-    {
-      path: 'produtos',
-      loadComponent: () =>
-        import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
-    },
-    {
-      path: 'carrinho',
-      loadComponent: () => import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
-    },
-    {
-      path: '**',
-      redirectTo: '',
-    },
-  ];
+import { authGuard } from './core/auth.guard';
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'produtos',
+    loadComponent: () =>
+      import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
+  },
+  {
+    path: 'carrinho',
+    canActivate: [authGuard],
+    loadComponent: () => import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
+  },
+
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
